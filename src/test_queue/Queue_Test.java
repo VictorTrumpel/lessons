2fec1954e39c3d78c94d0c8b8a55511902bd99e8
Victor Trumpel <victor.trumpel@gmail.com>
2022-12-10 15:22:56 +0300

    add Queue test

--- src/test_queue/Queue_Test.java
+++ src/test_queue/Queue_Test.java
@@ -6,11 +6,15 @@ import org.junit.Test;
 
 import queue.*;
 
+// Некорректное извлечение из непустой очереди
+
 public class Queue_Test {
 
   @Test
   public void test_1() {
     Queue<Integer> queue = new Queue<Integer>();
+
+    assertEquals(queue.dequeue(), null);
 
     assertEquals(queue.size(), 0);
     assertEquals(queue.dequeue(), null);
@@ -36,6 +40,42 @@ public class Queue_Test {
     assertEquals(queue.dequeue(), null);
 
     assertEquals(queue.size(), 0);
+  }
+
+  @Test
+  public void test_2() {
+    Queue<Integer> queue = new Queue<Integer>();
+
+    assertEquals(queue.dequeue(), null);
+    assertEquals(queue.size(), 0);
+
+    queue.enqueue(0);
+
+    assertEquals(queue.size(), 1);
+
+    assertEquals(queue.dequeue(), (Integer) 0);
+
+    assertEquals(queue.dequeue(), null);
+  }
+
+  @Test
+  public void test_3() {
+    Queue<Integer> queue = new Queue<Integer>();
+
+    assertEquals(queue.dequeue(), null);
+    assertEquals(queue.size(), 0);
+
+    for (int i = 0; i < 30; i++) {
+      queue.enqueue(i);
+    }
+
+    assertEquals(queue.size(), 30);
+
+    for (int i = 0; i < 30; i++) {
+      assertEquals(queue.size(), 30 - i);
+      assertEquals(queue.dequeue(), (Integer) i);
+      assertEquals(queue.size(), 30 - i - 1);
+    }
   }
 
 }
