e0ad195a327371df71bc35c6fc1cd89eba0dd5ca
Victor Trumpel <victor.trumpel@gmail.com>
2023-04-18 16:01:26 +0300

    add palindrom

---
+++ src/recursion/Palindrom.java
@@ -0,0 +1,31 @@
+package recursion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Palindrom {
+
+  public boolean isPalindrom(String str) {
+    if (str.length() == 1)
+      return true;
+
+    char c1 = str.charAt(0);
+    char c2 = str.charAt(str.length() - 1);
+    String newString = str.substring(1, str.length() - 1);
+
+    return c1 == c2 && isPalindrom(newString);
+  }
+
+  @Test
+  public void test_1() {
+    assertEquals(isPalindrom("aga"), true);
+
+    assertEquals(isPalindrom("agaa"), false);
+
+    assertEquals(isPalindrom("aggaa"), false);
+
+    assertEquals(isPalindrom("ротор"), true);
+  }
+
+}
