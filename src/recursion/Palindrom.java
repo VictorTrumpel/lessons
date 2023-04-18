312ce2c37c3b4ed4cdb8e7065c6da07c3b333651
Victor Trumpel <victor.trumpel@gmail.com>
2023-04-18 18:12:29 +0300

    optimize Palindrop

--- src/recursion/Palindrom.java
+++ src/recursion/Palindrom.java
@@ -14,7 +14,10 @@ public class Palindrom {
     char c2 = str.charAt(str.length() - 1);
     String newString = str.substring(1, str.length() - 1);
 
-    return c1 == c2 && isPalindrom(newString);
+    if (c1 != c2)
+      return false;
+
+    return isPalindrom(newString);
   }
 
   @Test
