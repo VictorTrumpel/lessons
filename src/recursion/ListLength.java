5ec19e7585a4b8770097d66bcb8ab9ec14e38390
Victor Trumpel <victor.trumpel@gmail.com>
2023-04-19 10:45:26 +0300

    add List commit

--- src/recursion/ListLength.java
+++ src/recursion/ListLength.java
@@ -2,82 +2,36 @@ package recursion;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
 import org.junit.Test;
 
-class Node {
-  public int value;
-  public Node next;
-  public Node prev;
+public class ListLength {
 
-  public Node(int _value) {
-    value = _value;
-    next = null;
-    prev = null;
-  }
-}
-
-class LinkedList {
-  public Node head;
-  public Node tail;
-
-  public LinkedList() {
-    head = null;
-    tail = null;
-  }
-
-  public void pop() {
-    if (head == null)
-      return;
-
-    head = head.next;
-  }
-
-  public int length() {
-    if (head == null)
+  public int length(List list) {
+    if (list.size() == 0)
       return 0;
 
-    pop();
+    list.remove(0);
 
-    return 1 + length();
+    return 1 + length(list);
   }
 
-  public void addInTail(Node _item) {
-    if (head == null) {
-      this.head = _item;
-      this.head.next = null;
-      this.head.prev = null;
-    } else {
-      this.tail.next = _item;
-      _item.prev = tail;
-    }
-    this.tail = _item;
-  }
-}
-
-public class ListLength {
   @Test
   public void test_1() {
-    LinkedList list = new LinkedList();
-    list.addInTail(new Node(1));
-    list.addInTail(new Node(2));
-    list.addInTail(new Node(3));
+    ArrayList<Integer> arrayList = new ArrayList<Integer>();
+    arrayList.add(1);
+    arrayList.add(2);
 
-    assertEquals(list.length(), 3);
+    assertEquals(length(arrayList), 2);
+    assertEquals(length(arrayList), 0);
 
-    assertEquals(list.length(), 0);
+    ArrayList<Integer> arrayList1 = new ArrayList<Integer>();
+    arrayList1.add(1);
+    arrayList1.add(2);
+    arrayList1.add(3);
 
-    list.addInTail(new Node(1));
-
-    assertEquals(list.length(), 1);
-
-    assertEquals(list.length(), 0);
-    assertEquals(list.length(), 0);
-
-    list.addInTail(new Node(1));
-    list.addInTail(new Node(2));
-    list.addInTail(new Node(3));
-    list.addInTail(new Node(4));
-
-    assertEquals(list.length(), 4);
+    assertEquals(length(arrayList1), 3);
+    assertEquals(length(arrayList1), 0);
   }
 }
